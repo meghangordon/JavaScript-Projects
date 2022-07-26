3c91ee58bd2dfc4b235112e45128522f6234d017
meghangordon <buffy10626@gmail.com>
2022-07-26 14:53:36 -0700

    Submitting bacis javascript projects, step 64.

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+var A = ("\"Be who you are say what you feel," // This is variable A
+    + " because those who mind don\'t matter and those who matter don\'t mind.\""
+    + "-Dr. Seuss");
+
+var B = "Meow" + " Bork" //This is variable B
+    
+var C = "The Gordons", Dad = "Duane", Mom = "Amie", //This is variable C
+    Daughter = "Meghan", Son = "Marc";
+
+var D = "13 + 31";
+
+
+document.write(D)
+
+window.alert("Click me!")    
