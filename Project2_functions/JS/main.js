3274443a2a9d8683514fef87e77913d469f0f959
meghangordon <buffy10626@gmail.com>
2022-07-27 17:20:45 -0700

    Pusing prject2_functions assignment, step 69.

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,14 @@
+function add(num1, n) {
+    return 
+}
+
+function changeColor(newColor) {
+    const elem = document.getElementById('para');
+    elem.style.color = newColor
+}
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this course!";
+    document.getElementById("Concatenate").innerHTML = sentence; 
+}
